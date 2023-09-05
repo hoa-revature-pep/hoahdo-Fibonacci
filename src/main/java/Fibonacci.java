5417924702ed7500ad36114092d577d0523aaa91
Hoa Do <hoa.do.ma@gmail.com>
2023-09-05 11:32:10 +0000

    Completed Challenge: Fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class Fibonacci {
     /**
@@ -15,6 +17,18 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        /**
+         * n: 0  1  2  3  4  5  6  7  8  9  10 11
+         * f: 0  1  1  2  3  5  8  13 21 34 55 89
+         */
+        List<Integer> fibo = new ArrayList<Integer>();
+        fibo.add(0);
+        fibo.add(1);
+
+        for (int i = 2; i <= n; i++) {
+            fibo.add(i, (fibo.get(i-1) + fibo.get(i-2)));
+        }
+
+        return fibo.get(n);
     }
 }
